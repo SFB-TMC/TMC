4a338ea97af6ffc39340db1fb7d1c7d3e9a2a7bd
SuperBigJJJJJam <543544868@qq.com>
2015-06-15 20:56:33 +0800

    Signed-off-by: SuperBigJJJJJam <543544868@qq.com>

--- dev/client/android/mainProject/src/org/sfbtmc/activity/AgendaActivity.java
+++ dev/client/android/mainProject/src/org/sfbtmc/activity/AgendaActivity.java
@@ -1,11 +1,13 @@
 package org.sfbtmc.activity;
 
 import org.sfbtmc.R;
-import org.sfbtmc.widget.MainPageMenuPopWindow;
+import org.sfbtmc.widget.AgendaMenuPopWindow;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AgendaActivity extends Activity {
 
@@ -13,6 +15,15 @@ public class AgendaActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_agenda);
+//		final WebView mWebView = new WebView(this);
+//		mWebView.setWebViewClient(new WebViewClient(){
+//			@Override
+//			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//				// TODO Auto-generated method stub
+//				mWebView.loadUrl("https://github.com/SFB-TMC/TMC/tree/master/doc");
+//				return true;
+//			}
+//		});
 	}
 
 	public void onClick(View view) {
@@ -21,7 +32,7 @@ public class AgendaActivity extends Activity {
 				super.onBackPressed();
 				break;
 			case R.id.layout_title_menu_img:
-				MainPageMenuPopWindow morePopWindow = new MainPageMenuPopWindow(this);
+				AgendaMenuPopWindow morePopWindow = new AgendaMenuPopWindow(this);
 				morePopWindow.showPopupWindow(view);
 				break;
 		}
